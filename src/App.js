a697072f74f83ad41787cad7e1efbd8a077b5c35
MedlynJacob <88194420+MedlynJacob@users.noreply.github.com>
2022-11-13 08:48:32 +0530

    setup gh-pages

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from "./components/Header";
+import Home from './components/Home';
+
+//http://localhost:3000/
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
